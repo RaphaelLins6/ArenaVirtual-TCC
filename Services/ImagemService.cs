adbb9f44960360df1ebc852538c04832c90c391b
Raphael Lins <rapharell06@gmail.com>
2025-08-05 01:28:56 -0300

    Adicionar método SalvarImagemAsync na classe ImagemService
    Implementa um método assíncrono que salva imagens em um diretório específico, gerando um novo nome de arquivo com GUID e criando o diretório se necessário. O método retorna o caminho completo da imagem salva.

---
+++ Services/ImagemService.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace ArenaVirtual.Services;
+
+public class ImagemService
+{
+    public async Task<string> SalvarImagemAsync(string caminhoOriginal)
+    {
+        string nomeArquivo = $"{Guid.NewGuid()}.jpg";
+        string destino = Path.Combine(FileSystem.AppDataDirectory, "Imagens");
+
+        if (!Directory.Exists(destino))
+            Directory.CreateDirectory(destino);
+
+        string caminhoCompleto = Path.Combine(destino, nomeArquivo);
+
+        File.Copy(caminhoOriginal, caminhoCompleto, overwrite: true);
+
+        return caminhoCompleto;
+    }
+}
