a697770ad7d7e0b5c30f69c5c586284ce15e2e74
Raphael Lins <rapharell06@gmail.com>
2025-08-05 22:27:38 -0300

    Correção de bugs e exceções para a plataforma android.

---
+++ Converters/StringNotNullOrEmptyConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace ArenaVirtual.Converters {
+    public class StringNotNullOrEmptyConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return !string.IsNullOrEmpty(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException("StringNotNullOrEmptyConverter não suporta conversão reversa.");
+        }
+    }
+}
+
